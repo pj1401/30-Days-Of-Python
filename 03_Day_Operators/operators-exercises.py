4fe66a2e769165cbce9e8bc08f1f552a991160ea
Patricia Johansson <pj222uc@student.lnu.se>
2026-02-20 17:02:23 +0100

    Add operators-exercises.py

---
+++ 03_Day_Operators/operators-exercises.py
@@ -0,0 +1,16 @@
+age = 23
+height = 168.9
+complex_number = 2 + 1j
+
+triangle_base = float(input('Enter base: '))
+triangle_height = float(input('Enter height: '))
+print('The area of the triangle is ', 0.5 * triangle_base * triangle_height)
+
+triangle_side_a = float(input('Enter side a:'))
+triangle_side_b = float(input('Enter side b: '))
+triangle_side_c = float(input('Enter side c: '))
+print('The perimeter of the triangle is ', triangle_side_a + triangle_side_b + triangle_side_c)
+
+rectangle_length = float(input('Enter rectangle length: '))
+rectangle_width = float(input('Enter rectangle width: '))
+print('The area of the rectangle is ', rectangle_length * rectangle_width, '\nThe perimeter of the rectangle is ', 2 * (rectangle_length + rectangle_width))
